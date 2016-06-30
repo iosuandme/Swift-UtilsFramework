16cc31137fd9a8f6dfa2bc60e0610eb2c77a2493
慧趣小歪 <huiquhome@163.com>
2016-06-30 20:44:36 +0800

    新增值变化监听通知

---
+++ SwiftFrameworks/extension/Listener.swift
@@ -0,0 +1,53 @@
+//
+//  Listener.swift
+//  TallyCloud
+//
+//  Created by 慧趣小歪 on 16/6/30.
+//  Copyright © 2016年 小分队. All rights reserved.
+//
+//  通知监听器
+//
+
+import Foundation
+
+public class Listener<T> {
+    
+    private var notifications:[Notification<T>] = []
+    
+    public func addNotificationBy(target target:AnyObject, callback:(T)->Void) {
+        notifications.append(Notification<T>(target, callback))
+        callback(onInitNotification())
+    }
+    
+    public func dispatchChanged(item:T) {
+        notifications.forEach { $0.callback(item) }
+    }
+    
+    public func removeNotificationBy(target target:AnyObject) {
+        for i in (0..<notifications.count).reverse() {
+            if notifications[i].target === target {
+                notifications.removeAtIndex(i)
+            }
+        }
+    }
+    
+    private var onInitNotification:()->T
+    init(onInitNotification:()->T) {
+        self.onInitNotification = onInitNotification
+    }
+    
+}
+
+
+private class Notification<T> {
+    
+    private var target:AnyObject
+    
+    private var callback:(T)->Void
+    
+    init(_ target:AnyObject, _ callback:(T)->Void) {
+        self.target = target
+        self.callback = callback
+    }
+    
+}
