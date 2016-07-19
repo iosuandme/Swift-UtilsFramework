fb143bbda2cb1bc8d231dd24ad12c1038f7f1e0c
慧趣小歪 <huiquhome@163.com>
2016-07-19 20:21:31 +0800

    增加注释

--- SwiftFrameworks/extension/Listener.swift
+++ SwiftFrameworks/extension/Listener.swift
@@ -16,6 +16,8 @@ public class Listener<T> {
     
     public var count:Int { return notifications.count }
     
+    
+    /// 闭包内不要使用self
     public func addNotificationBy(target target:AnyObject, callback:(T)->Void) {
         notifications.append(Notification<T>(target, callback))
         if let item:T = onInitNotification?() {
